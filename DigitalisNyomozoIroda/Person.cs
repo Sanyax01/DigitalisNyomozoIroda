936a4c29120f85be09fceae070fe0469cb0dca32
Sanyi <SanyaxYT01@gmail.com>
2026-01-30 09:46:44 +0100

    Person class kész tostringgel

--- DigitalisNyomozoIroda/Person.cs
+++ DigitalisNyomozoIroda/Person.cs
@@ -8,5 +8,26 @@ namespace DigitalisNyomozoIroda
 {
 	internal class Person
 	{
+		private string nev;
+		private int eletkor;
+		private string megjegyzes;
+
+		public Person(string nev, int eletkor, string megjegyzes)
+		{
+			this.nev = nev;
+			this.eletkor = eletkor;
+			this.megjegyzes = megjegyzes;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Eletkor { get => eletkor; set => eletkor = value; }
+		public string Megjegyzes { get => megjegyzes; set => megjegyzes = value; }
+
+
+		public override string ToString()
+		{
+			return $"{this.nev} - {this.eletkor} év\nMegjegyzés: {this.megjegyzes}";
+		}
 	}
+
 }
