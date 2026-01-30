0baf64615423e905a98b7db8c5bf712106f9c84c
Sanyi <SanyaxYT01@gmail.com>
2026-01-30 08:46:20 +0100

    Add project files.

---
+++ DigitalisNyomozoIroda/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DigitalisNyomozoIroda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
