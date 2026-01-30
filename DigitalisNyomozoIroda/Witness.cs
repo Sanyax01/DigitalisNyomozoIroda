4c90b474c6846bf11604b3f46fec12647bc60a37
Sanyi <SanyaxYT01@gmail.com>
2026-01-30 09:59:44 +0100

    Witness class kesz tostringgel

--- DigitalisNyomozoIroda/Witness.cs
+++ DigitalisNyomozoIroda/Witness.cs
@@ -8,5 +8,25 @@ namespace DigitalisNyomozoIroda
 {
 	internal class Witness
 	{
+		private Person szemtanu;
+		private string vallomas;
+		private string datum;
+
+		public Witness(Person szemtanu, string vallomas, string datum)
+		{
+			this.szemtanu = szemtanu;
+			this.vallomas = vallomas;
+			this.datum = datum;
+		}
+
+		public string Vallomas { get => vallomas; set => vallomas = value; }
+		public string Datum { get => datum; set => datum = value; }
+		internal Person Szemtanu { get => szemtanu; set => szemtanu = value; }
+
+
+		public override string ToString()
+		{
+			return $"{this.szemtanu.ToString()}\nVallomás: {this.vallomas}\n\tDátum: {this.datum}";
+		}
 	}
 }
