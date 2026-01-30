a978a84b44c44a95540b8e061e35d0168b5a9900
Sanyi <SanyaxYT01@gmail.com>
2026-01-30 10:47:56 +0100

    EvidenceManager class kesz

--- DigitalisNyomozoIroda/EvidenceManager.cs
+++ DigitalisNyomozoIroda/EvidenceManager.cs
@@ -8,5 +8,52 @@ namespace DigitalisNyomozoIroda
 {
 	internal class EvidenceManager
 	{
+		List<Evidence> bizonyitekok;
+		List<Evidence> raktar;
+
+		public EvidenceManager ()
+		{
+			bizonyitekok = new List<Evidence> ();
+			raktar = new List<Evidence> ();
+
+		}
+
+
+		public void bizonyitekHozaadasa(Evidence bizonyitek)
+		{
+			if (bizonyitek != null) 
+			{
+				if (raktar.Contains(bizonyitek))
+				{
+					raktar.Remove(bizonyitek);
+					bizonyitekok.Add(bizonyitek);
+
+				}
+				else if (bizonyitekok.Contains(bizonyitek))
+				{
+					Console.WriteLine("Ez a bizonyiték már létezik!");
+
+				}
+				else
+				{
+					bizonyitekok.Add(bizonyitek);
+				}
+			}
+			
+		}
+
+		public Evidence bizonyitekTorlese(Evidence item)
+		{
+			if (bizonyitekok.Contains(item))
+			{
+				raktar.Add(item);
+				bizonyitekok.Remove(item);
+			}
+			else 
+			{
+				Console.WriteLine("Ez a bizonyiték nem létezik!"); 
+			}
+			return item;
+		}
 	}
 }
