03d85776a655671af245f4c9989d6b7a0455d9e4
Sanyi <SanyaxYT01@gmail.com>
2026-01-30 10:59:24 +0100

    CaseManager class hozarendeleos metodusok nelkul

--- DigitalisNyomozoIroda/CaseManager.cs
+++ DigitalisNyomozoIroda/CaseManager.cs
@@ -8,5 +8,32 @@ namespace DigitalisNyomozoIroda
 {
 	internal class CaseManager
 	{
+		List<Case> ugyek;
+
+		public CaseManager() 
+		{
+			ugyek = new List<Case>();
+		}
+
+		public void UgyLetrehozas(Case ugy)
+		{
+			if (ugy != null)
+			{
+				ugyek.Add(ugy);
+			}
+
+		}
+
+		public void UgyListazas()
+		{
+			foreach (var item in ugyek)
+			{
+				Console.WriteLine(item);
+				Console.WriteLine("-----------------------");
+			}
+		
+		}
+
+
 	}
 }
