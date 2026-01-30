ebd98d967773dfda474c7d586c26d5b00ca3a5b9
Sanyi <SanyaxYT01@gmail.com>
2026-01-30 09:50:28 +0100

    Suspect class kész tostringgel

--- DigitalisNyomozoIroda/Suspect.cs
+++ DigitalisNyomozoIroda/Suspect.cs
@@ -8,5 +8,21 @@ namespace DigitalisNyomozoIroda
 {
 	internal class Suspect
 	{
+		private Person gyanusitott;
+		private int gyanusszint;
+
+		public Suspect(Person gyanusitott, int gyanusszint)
+		{
+			this.gyanusitott = gyanusitott;
+			this.gyanusszint = gyanusszint;
+		}
+
+		public int Gyanusszint { get => gyanusszint; set => gyanusszint = value; }
+		internal Person Gyanusitott { get => gyanusitott; set => gyanusitott = value; }
+
+		public override string ToString()
+		{
+			return $"{this.gyanusitott.Nev} - {this.gyanusitott.Eletkor}\nGyanúsítottsági szint(0-100): {this.gyanusszint}";
+		}
 	}
 }
