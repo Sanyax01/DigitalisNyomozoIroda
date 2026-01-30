37778578d3ca6a575a89a10f3e07ef0f3640b44a
Sanyi <SanyaxYT01@gmail.com>
2026-01-30 09:09:19 +0100

    Case class kesz

--- DigitalisNyomozoIroda/Case.cs
+++ DigitalisNyomozoIroda/Case.cs
@@ -8,5 +8,27 @@ namespace DigitalisNyomozoIroda
 {
 	internal class Case
 	{
+		private string ugyazonosito;
+		private string cim;
+		private string leiras;
+		private string allapot;
+		private List<Person> szemelyek;
+		private List<Evidence> bizonyitekok;
+		
+
+		public Case(string ugyazonosito, string cim, string leiras, string allapot)
+		{
+			this.ugyazonosito = ugyazonosito;
+			this.cim = cim;
+			this.leiras = leiras;
+			this.allapot = allapot;
+			this.szemelyek = new List<Person>();
+			this.bizonyitekok = new List<Evidence>();
+		}
+
+		public string Ugyazonosito { get => ugyazonosito; set => ugyazonosito = value; }
+		public string Cim { get => cim; set => cim = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+		public string Allapot { get => allapot; set => allapot = value; }
 	}
 }
