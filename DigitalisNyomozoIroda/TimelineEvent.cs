708adf80032aab26535d64b810db5b4f3ef4ef9c
Sanyi <SanyaxYT01@gmail.com>
2026-01-30 10:22:17 +0100

    TimelineEvent calss kesz tostringgel

--- DigitalisNyomozoIroda/TimelineEvent.cs
+++ DigitalisNyomozoIroda/TimelineEvent.cs
@@ -8,5 +8,21 @@ namespace DigitalisNyomozoIroda
 {
 	internal class TimelineEvent
 	{
+		private string datum;
+		private string esemeny;
+
+		public TimelineEvent(string datum, string esemeny)
+		{
+			this.datum = datum;
+			this.esemeny = esemeny;
+		}
+
+		public string Datum { get => datum; set => datum = value; }
+		public string Esemeny { get => esemeny; set => esemeny = value; }
+
+		public override string ToString() 
+		{
+			return $"Dátum:{this.datum} esemény:{this.esemeny}";
+		}
 	}
 }
